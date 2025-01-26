55c791d2d4e2ab60f33defeb9110710a4bea744f
realHex <sajitha.20220784@iit.ac.lk>
2025-01-26 17:10:13 +0530

    Added Convex-Clerk connectivity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-providers";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +43,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ConvexClientProvider>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -50,6 +52,7 @@ export default function RootLayout({
           storageKey="notion-theme-2">
           {children}
         </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://informed-stork-10.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
